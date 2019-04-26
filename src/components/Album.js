2c87bcab633e85b93560b67ed00b916548b32bd4
Jacy-Barger <barger.jacy@gmail.com>
2019-04-26 07:48:05 -0500

    Added a table to the album component displaying each song number, title, and duration

--- src/components/Album.js
+++ src/components/Album.js
@@ -4,7 +4,7 @@ import albumData from './../data/albums';
 class Album extends Component {
   constructor(props) {
     super(props);
-  
+
 
 const album = albumData.find( album => {
     return album.slug === this.props.match.params.slug
@@ -27,15 +27,24 @@ const album = albumData.find( album => {
            <div id="release-info">{this.state.album.releaseInfo}</div>
          </div>
         </section>
-         <table id="song-list">
+        <table id="song-list">
           <colgroup>
             <col id="song-number-column" />
             <col id="song-title-column" />
             <col id="song-duration-column" />
           </colgroup>
-         <tbody>
-        </tbody>
-      </table>
+          <tbody>
+          {
+            this.state.album.songs.map( (song, index) =>
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
+              </tr>
+          )
+        }
+          </tbody>
+        </table>
        </section>
      );
    }
